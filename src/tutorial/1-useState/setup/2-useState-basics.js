0db598a0a83aa5ad82127f250066cc57bc44a7b6
Mark Abrahams <mark001616@gmail.com>
2022-04-14 13:05:25 -0500

    got the basics for useState basics

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+  // the [] can be named anything, its named text here.
+  const [text, setText] = useState("Random Title");
+
+  const handleClick = () => {
+    // acts as a toggle, if the button gets pressed again it will
+    // return back the original title.
+    if (text == "random title") {
+      setText("hello world");
+    } else {
+      setText("random title");
+    }
+  };
+  return (
+    <React.Fragment>
+      <h1>{text} </h1>
+      <button className="btn" onClick={handleClick}>
+        change title
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
