1e4c0e73d48de5e991efa42da7acb6f7ca670aaf
Mark Abrahams <mark001616@gmail.com>
2022-04-16 12:37:28 -0500

    counter example

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const [name, setName] = useState("Peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
+  const changeMessage = () => {
+    // for objects, to make sure other values are not erased.
+    // you need to use the {..., value}
+    setPerson({ ...person, message: "Hello world" });
+    //setMessage("this is a test");
+  };
+  console.log(person);
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
